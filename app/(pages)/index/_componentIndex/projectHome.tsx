048f3646ffe1e3bfa7024e4549722f68c453e39e
Aihos <hugoleray53@gmail.com>
2025-12-08 18:02:29 +0100

    changement et correction projecthome et svg

--- app/(pages)/index/_componentIndex/projectHome.tsx
+++ app/(pages)/index/_componentIndex/projectHome.tsx
@@ -6,6 +6,7 @@ import Button from "../../_components/Button";
 import Bluestarsvg from "../../_components/svg/bluestar";
 import CarrouselProject from "../../home/_components/carrouselProject";
 import LineProject from "./lineProject";
+import Link from "next/link";
 
 // Registrer le plugin ScrollTrigger
 if (typeof window !== "undefined") {
@@ -98,9 +99,14 @@ export default function ProjectHome(){
             ScrollTrigger.getAll().forEach(trigger => trigger.kill());
         };
     }, []);
-
+/* 
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+ */
     return(
-          <div ref={containerRef} className="px-4 sm:px-8 md:px-12 lg:px-[68px] relative min-h-[130vh] bg-primary h-full w-full pt-20">         
+          <div ref={containerRef} className="flex flex-col justify-evenly items-center px-4 sm:px-8 md:px-12 lg:px-[68px] relative min-h-[130vh] bg-primary h-full w-full pt-0">         
             <div ref={headerContentRef} className="flex flex-col md:flex-row w-full justify-between gap-4 md:gap-0">
                 <div>
                     <h2 ref={titleRef} className="text-white uppercase text-4xl sm:text-6xl lg:text-8xl font-beckam">Projets</h2>
@@ -109,8 +115,8 @@ export default function ProjectHome(){
                     </div>
                    {/*  <p ref={textRef} className="max-w-1/3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati vero possimus nesciunt tempora fuga eius nam officiis, unde accusamus repellat!</p>
               */}   </div>
-                <div ref={buttonRef} className="hidden md:block">
-                    <Button url="/galerie" texte="Voir plus" />
+                <div ref={buttonRef} className="hidden md:block p-1">
+                    <Link className="border-white  border-1 border-double px-16 mt-4 button py-4 uppercase text-sm  transition-all text-white hover:bg-white hover:text-[#2A2EC5]" href="/galerie" >Voir plus</Link>
                 </div>
             </div>
             <div ref={carrouselRef}>
